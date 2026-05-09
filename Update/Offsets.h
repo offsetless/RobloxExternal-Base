0ab35e4ba843ef58b59151b4a35e6a6a70b38d71
cupid <s0ulz.ven@gmail.com>
2026-05-09 05:14:34 -0400

    Update Offsets.h

--- Update/Offsets.h
+++ Update/Offsets.h
@@ -1,4 +1,4 @@
-//v-bf6344c9c23446bf offset, if this is not ur version then ur just gonna have to upd this
+//version-bf6344c9c23446bf, if this is not ur RBX version then ur just gonna have to upd this
 namespace Offsets{
   inline constexpr uintptr_t Walkspeed = 0x1dc;
 }
